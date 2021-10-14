fa870d1236d0f92bdf652bf5bc964239c2d9a403
mcalijr <martink.moc@gmail.com>
2021-10-15 01:13:36 +0200

    JS side 100% finished (hopefully haha) <-- still need to add localStorage however it is a topic I eyt have to learn, so that's for next seesion. Other than that, finally gonna take care of styling the page during next coding session

--- app.js
+++ app.js
@@ -9,16 +9,11 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-// ! unnecessary
-Book.prototype.info = function () {
-  return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
-};
-
 // todo delete this later
-const newBook1 = new Book("Hobbit", "Tolkien", 758, "read already");
+const newBook1 = new Book("Hobbit", "Tolkien", 758, "❌");
 myLibrary.push(newBook1);
 
-const newBook2 = new Book("Fx God", "MartinK", 420, "read already");
+const newBook2 = new Book("Fx God", "MartinK", 420, "✔️");
 myLibrary.push(newBook2);
 // todo delete to here
 
@@ -46,29 +41,37 @@ function addBookToLibrary() {
 let list = document.querySelector(".list");
 
 function displayTable() {
+  // Delete previous table with each update
   list.innerHTML = "";
   createTableHeader();
 
+  // Create HTML elements and append them to create new table row
   myLibrary.forEach(function (book, index) {
     let tr = document.createElement("tr");
     let td1 = document.createElement("td");
     let td2 = document.createElement("td");
     let td3 = document.createElement("td");
     let td4 = document.createElement("td");
+    let td5 = document.createElement("td");
     let textContent1 = document.createTextNode(book.title);
     let textContent2 = document.createTextNode(book.author);
     let textContent3 = document.createTextNode(book.pages);
     let textContent4 = document.createTextNode(book.read);
+    let textContent5 = document.createElement("button");
     list.appendChild(tr);
     tr.appendChild(td1);
     tr.appendChild(td2);
     tr.appendChild(td3);
     tr.appendChild(td3);
-    tr.appendChild(td4);
+    tr.appendChild(td4).classList.add("tickOrX");
+    tr.appendChild(td4).setAttribute("data-index", index);
+    tr.appendChild(td5);
     td1.appendChild(textContent1);
     td2.appendChild(textContent2);
     td3.appendChild(textContent3);
     td4.appendChild(textContent4);
+    td5.appendChild(textContent5).classList.add("deleteButton");
+    td5.appendChild(textContent5).setAttribute("data-index", index);
   });
 }
 
@@ -78,29 +81,73 @@ function createTableHeader() {
   let th2 = document.createElement("th");
   let th3 = document.createElement("th");
   let th4 = document.createElement("th");
+  let th5 = document.createElement("th");
   let headerContent1 = document.createTextNode("Title");
   let headerContent2 = document.createTextNode("Author");
   let headerContent3 = document.createTextNode("Pages");
   let headerContent4 = document.createTextNode("Finished?");
+  let headerContent5 = document.createTextNode("Delete");
 
   list.appendChild(tr);
   tr.appendChild(th1);
   tr.appendChild(th2);
   tr.appendChild(th3);
   tr.appendChild(th4);
+  tr.appendChild(th5);
 
   th1.appendChild(headerContent1);
   th2.appendChild(headerContent2);
   th3.appendChild(headerContent3);
   th4.appendChild(headerContent4);
+  th5.appendChild(headerContent5);
 }
 
 displayTable();
+
+Book.prototype.changeSign = function () {
+  return this.read == "✔️" ? (this.read = "❌") : (this.read = "✔️");
+};
+
+function letUserChangeReadStatus() {
+  const tickOrX = Array.from(document.querySelectorAll(".tickOrX"));
+  tickOrX.forEach((tickOrX) => {
+    tickOrX.addEventListener("click", () => {
+      console.log(tickOrX.dataset.index);
+      myLibrary[tickOrX.dataset.index].changeSign();
+      displayTable();
+      defineLibraryArray();
+      letUserChangeReadStatus();
+      resetDeleteAbility();
+    });
+  });
+}
+letUserChangeReadStatus();
+
+function defineLibraryArray() {
+  deleteButton = Array.from(document.querySelectorAll(".deleteButton"));
+}
+defineLibraryArray();
+
+function resetDeleteAbility() {
+  deleteButton.forEach((button) => {
+    button.addEventListener("click", () => {
+      myLibrary.splice(button.dataset.index, 1);
+      displayTable();
+      defineLibraryArray();
+      resetDeleteAbility();
+      letUserChangeReadStatus();
+    });
+  });
+}
+resetDeleteAbility();
 
 const addBookClick = document.querySelector(".submitBtn");
 addBookClick.addEventListener("click", function () {
   addBookToLibrary();
   displayTable();
+  defineLibraryArray();
+  resetDeleteAbility();
+  letUserChangeReadStatus();
   popup.classList.toggle("active");
 });
 
